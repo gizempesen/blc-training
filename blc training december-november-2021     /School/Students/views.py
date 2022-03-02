980a630c7e18a98b41f17021d46e45c406e38efc
gizem <pesengizem999@gmail.com>
2022-03-02 20:40:16 +0300

    kubespray

--- blc training december-november-2021     /School/Students/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from . import models
-
-# Create your views here.
-def listStudents(request):
-    models.Students.objects.create(name="test", surname="test" , email = "pesengizem@gmail.com" , age = 19, number = 67, grade = 7834)
-    return render(request,'list.html', {'data_students': models.Students.objects.all()})
